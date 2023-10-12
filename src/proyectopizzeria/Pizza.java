3118b6f48886e75fac98bfe42deb29edfcd7e2ce
Agustin Kloberdanz <akloberdanz399@gmail.com>
2023-10-12 11:06:04 -0300

    Agrega entidad Pizza y crea array donde se guardara el pedido

---
+++ src/proyectopizzeria/Pizza.java
@@ -0,0 +1,39 @@
+package proyectopizzeria;
+
+public class Pizza {
+
+    private String nombre;
+    private int precio;
+    private int cantidad;
+
+    public Pizza(String nombre, int precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getPrecio() {
+        return this.precio;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+}
